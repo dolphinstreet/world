ec14970228d4065c777633bd59f2726bb937e931
dolphinstreet <delfinamarchetti@hotmail.it>
2023-08-17 18:00:25 +0200

    endGame(), startGame(), modal for game end

--- script.js
+++ script.js
@@ -2,39 +2,45 @@ let countries = document.querySelectorAll(".land");
 let activeCountry = null;
 let input = document.querySelector("#guessBar");
 let score = document.querySelector("#score");
-let totalCountries=document.querySelector("#totalCountries");
+let totalCountries = document.querySelector("#totalCountries");
 let guessedCountries = 0;
 let timer = document.getElementById("timer");
 
-input.disabled = true;
-countries.forEach(country => {
-    
-    timerFunction(20, timer)
-    country.addEventListener("click", () => {
-        input.disabled = false;
-        input.focus()
+const dialog = document.getElementById("endGameDialog");
+const dialogMessage = document.getElementById("dialogMessage");
+const dialogRestartButton = document.getElementById("restartButton");
 
-        if (activeCountry) {
-            activeCountry.classList.remove("active");
-            clearInput();
-        }
-        if (!country.classList.contains("win")) {
-            activeCountry = country;
-            country.classList.toggle("active")
-        }
-        setTimeout(() => {
-            input.addEventListener("keyup", () => {
-                clearTimeout()
-                if (input.value === activeCountry.getAttribute("title")) {
-                    activeCountry.classList.remove("active")
-                    activeCountry.classList.add("win")
-                    updateScore();
-                    clearInput();
-                }
-            })
-        }, 200);
-    })
-});
+input.disabled = true;
+function startGame() {
+    countries.forEach(country => {
+
+        timerFunction(2, timer)
+        country.addEventListener("click", () => {
+            input.disabled = false;
+            input.focus()
+
+            if (activeCountry) {
+                activeCountry.classList.remove("active");
+                clearInput();
+            }
+            if (!country.classList.contains("win")) {
+                activeCountry = country;
+                country.classList.toggle("active")
+            }
+            setTimeout(() => {
+                input.addEventListener("keyup", () => {
+                    clearTimeout()
+                    if (input.value === activeCountry.getAttribute("title")) {
+                        activeCountry.classList.remove("active")
+                        activeCountry.classList.add("win")
+                        updateScore();
+                        clearInput();
+                    }
+                })
+            }, 200);
+        })
+    });
+}
 
 function clearInput() {
     input.value = ""
@@ -43,17 +49,62 @@ function updateScore() {
     guessedCountries++
     score.innerHTML = guessedCountries;
     totalCountries.innerHTML = countries.length;
+    if (guessedCountries===totalCountries){
+        endGame("You won")
+    }
 }
 
-function timerFunction(minutes, timer ){
-    let seconds= minutes*60;
-    let counter =""
-    setInterval(() => {
-        counter=`${Math.floor(seconds/60)}:${seconds%60}`;
-        timer.innerHTML=counter;
-        seconds--
+function timerFunction(minutes, timer) {
+    let seconds = minutes * 60;
+    let counter = ""
+    let intervalId = setInterval(() => {
+        counter = `${Math.floor(seconds / 60)}:${seconds % 60}`;
+        timer.innerHTML = counter;
+        if (seconds === 0) {
+            endGame("Time's up !");
+            clearInterval(intervalId);
+        } else {
+            seconds--
+        }
     }, 1000);
+
 }
+
+function endGame(message) {
+    dialog.close()
+    showModal(message);
+    clearInput();
+    input.disabled = true;
+}
+
+function showModal(message) {
+    dialogMessage.textContent = `${message} Score: ${guessedCountries}/${countries.length}`;
+    dialog.showModal();
+    dialogRestartButton.style.display = "block";
+}
+
+dialogRestartButton.addEventListener("click", () => {
+    dialog.close();
+    restartGame();
+});
+
+function restartGame() {
+    //reset variables
+    activeCountry = null;
+    guessedCountries = 0;
+    // reset states
+    countries.forEach(country => {
+        country.classList.remove("active", "win");
+    });
+    clearInput()
+    input.disabled = true;
+    //reset score
+    score.innerHTML = guessedCountries;
+    //start new game
+    startGame();
+}
+
+startGame();
 
 
 
